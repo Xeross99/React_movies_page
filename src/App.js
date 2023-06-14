fac08311e98049a1a9664797b43f24572d725451
Xeross99 <m.krzysteczko@icloud.com>
2023-06-14 18:31:46 +0200

    Add-pages

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
 import React from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import HomePage from './pages/HomePage/HomePage'
+import DetailsPage from './pages/DetailsPage/DetailsPage'
+import SearchPage from './pages/SearchPage/SearchPage'
+import ActorPage from './pages/ActorPage/ActorPage'
+
 
 const App = () => {
   return (
-    <div>App</div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={< HomePage />} />
+        <Route path="/details" element={< DetailsPage />} />
+        <Route path="/search" element={< SearchPage />} />
+        <Route path="/actor/:name" element={< ActorPage />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
---
+++ src/pages/ActorPage/ActorPage.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import styles from './ActorPage.module.css'
+
+const ActorPage = () => {
+  return (
+    <div>ActorPage</div>
+  )
+}
+
+export default ActorPage
